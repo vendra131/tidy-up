dd7044265b11d97df2caf6321dd7b5e179937e9e
Árpád Magosányi <mag@magwas.rulez.org>
2019-09-26 08:40:37 +0100

    Analysis is signed off (#4)
    * integrating to toolchain
    
    * kkpipeline integration added
    
    * latest greatest versions, no key generation
    
    * Create architecture model
    
    * Add registration architecture model
    
    * use staged constraints
    
    * policies, process containment
    
    * Define process step order on revoke. (#3)

---
+++ Makefile
@@ -0,0 +1,7 @@
+export MODEL_BASENAME=tidyup
+export REPO_NAME=tidy-up
+export GITHUB_ORGANIZATION=kode-konveyor
+export SONAR_ORG=$(GITHUB_ORGANIZATION)
+
+include /usr/local/toolchain/rules.java
+
---
+++ src/integration-test/java/com/kodekonveyor/tidyup/DummyIntegrationTest.java
@@ -0,0 +1,19 @@
+package com.kodekonveyor.tidyup;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@DisplayName("dummy behaviour")
+@SpringBootTest
+public class DummyIntegrationTest {
+	@InjectMocks
+	private DummyService dummyService;
+
+	@Test
+	public void test() {
+		Assertions.assertTrue(dummyService.call());
+	}
+}
