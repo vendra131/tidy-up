9c0840024053b8d47a2a776e455cf7e2e7b25589
Tamas Szirbucz <szirbucz@kodekonveyor.com>
2020-03-17 22:29:00 +0100

    Fix coverage

--- src/main/java/com/kodekonveyor/logging/LoggingMarkers.java
+++ src/main/java/com/kodekonveyor/logging/LoggingMarkers.java
@@ -3,9 +3,11 @@ package com.kodekonveyor.logging;
 import org.slf4j.Marker;
 import org.slf4j.MarkerFactory;
 
+import com.kodekonveyor.webapp.ExcludeFromCodeCoverage;
 import com.kodekonveyor.webapp.InterfaceClass;
 
 @InterfaceClass
+@ExcludeFromCodeCoverage("No methods in the class.")
 public class LoggingMarkers {
 
   public static final Marker AUTHENTICATION =
