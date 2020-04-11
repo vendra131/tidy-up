aa167f8f10edea5c871233a90febd0a5ca8cf101
Árpád Magosányi <m4gw4s@gmail.com>
2020-04-11 23:04:13 +0200

    version 0.3 of pmd rules (#131)
    * version 0.3 of pmd rules

---
+++ src/test/java/com/kodekonveyor/exception/ExceptionConstants.java
@@ -0,0 +1,15 @@
+package com.kodekonveyor.exception;
+
+public class ExceptionConstants {
+
+  public static final String EXPECTED_S_BUT_GOT_S = "expected %s but got %s";
+  public static final String NO_EXCEPTION_THROWN = "no exception thrown";
+  public static final String MESSAGE_DOES_NOT_CONTAIN =
+      "message does not contain: ";
+  public static final String GOT = "\n got:";
+  public static final String MESSAGE_DOES_NOT_MATCH_EXPECTED =
+      "message does not match. \nexpected: ";
+  public static final String NO_MESSAGE_OF_THE_EXCEPTION =
+      "no message of the exception";
+
+}
--- src/test/java/com/kodekonveyor/webapp/WebappTestHelper.java
+++ src/test/java/com/kodekonveyor/webapp/WebappTestHelper.java
@@ -2,13 +2,17 @@ package com.kodekonveyor.webapp;
 
 import static org.assertj.core.api.Assertions.fail;
 
+import java.text.MessageFormat;
+
 @InterfaceClass
 public class WebappTestHelper {
+
+  private static final String NO_0_IN_1 = "no {0} in {1}";
 
   public static void
       assertContains(final String contained, final String container) {
     if (!container.contains(contained))
-      fail("no " + contained + " in " + container);
+      fail(MessageFormat.format(NO_0_IN_1, contained, container));
   }
 
 }
