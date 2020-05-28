fbe72658c812e8e33ef5218dc835dbeb5997ec83
Anish Kumar Sarangi <61626468+anish97IND@users.noreply.github.com>
2020-05-28 11:56:12 +0530

    OpenWorkRequestController/Logging/issue#98 (#135)

--- src/main/java/com/kodekonveyor/work_request/open/OpenWorkRequestController.java
+++ src/main/java/com/kodekonveyor/work_request/open/OpenWorkRequestController.java
@@ -2,6 +2,7 @@ package com.kodekonveyor.work_request.open;
 
 import java.util.List;
 
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,10 +27,15 @@ public class OpenWorkRequestController {
   @Autowired
   AuthenticatedUserService authenticatedUserService;
 
+  @Autowired
+  Logger loggerService;
+
   @GetMapping("/workRequest/own/@workRequestId")
   public WorkRequestDTO call(@RequestParam final long workRequestId) {
+    loggerService.info(
+        WorkRequestConstants.SERVICE_CALL_NAME, super.getClass().getName()
+    );
     inputValidation(workRequestId);
-
     final WorkRequestEntity workRequestEntity =
         workRequestRepository.findByWorkRequestId(workRequestId).get(0);
 
@@ -41,28 +47,49 @@ public class OpenWorkRequestController {
       ) && !(currentUser.equals(workRequestEntity.getCustomer()) ||
           currentUser.equals(workRequestEntity.getProvider()))
 
-    )
+    ) {
+      loggerService.warn(
+          WorkRequestConstants.WORK_REQUEST_ERROR,
+          WorkRequestConstants.WORK_REQUEST_IS_NOT_POSTED,
+          WorkRequestConstants.FAILURE
+      );
       throw new ValidationException(
           WorkRequestConstants.WORK_REQUEST_IS_NOT_POSTED
       );
+    }
+    loggerService.debug(
+        WorkRequestConstants.OPEN_WORK_REQUEST,
+        WorkRequestConstants.SUCCESS
+    );
     return WorkRequestUtil.convertWorkRequestEntityToDTO(workRequestEntity);
   }
 
   private void inputValidation(final long workRequestId) {
 
     final int workId = 0;
-    if (workRequestId <= workId)
+    if (workRequestId <= workId) {
+      loggerService.warn(
+          WorkRequestConstants.INPUT_VALIDATION_STATUS,
+          WorkRequestConstants.NON_POSITIVE_WORK_REQUEST_ID_EXCEPTION,
+          WorkRequestConstants.FAILURE
+      );
       throw new ValidationException(
           WorkRequestConstants.NON_POSITIVE_WORK_REQUEST_ID_EXCEPTION
       );
+    }
 
     final List<WorkRequestEntity> workRequestEntity =
         workRequestRepository.findByWorkRequestId(workRequestId);
-    if (workRequestEntity.isEmpty())
+    if (workRequestEntity.isEmpty()) {
+      loggerService.warn(
+          WorkRequestConstants.INPUT_VALIDATION_STATUS,
+          WorkRequestConstants.INVALID_WORK_REQUEST_ID_EXCEPTION,
+          WorkRequestConstants.FAILURE
+      );
       throw new ValidationException(
           WorkRequestConstants.INVALID_WORK_REQUEST_ID_EXCEPTION
       );
-
+    }
   }
 
 }
